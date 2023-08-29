29e988e038922be4a9d1e6250ac3670606e74437
Daniel Jähnichen <danieljahnichen@Daniels-Mini.fritz.box>
2023-08-29 02:24:38 +0200

    Minor style changes

--- site/templates/character.php
+++ site/templates/character.php
@@ -67,14 +67,14 @@
               </div>
 
               <div class="grid grid-cols-1 gap-1 mb-12 -mx-6 md:gap-2 md:grid-cols-2 lg:grid-cols-4 md:mx-0">
-                <div x-show="character.data.gender" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Gender:</strong> <span x-text="character.data.gender"></span></div>
-                <div x-show="character.data.age" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Age:</strong> <span x-text="character.data.age"></span></div>
-                <div x-show="character.data.race.fullName" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Race:</strong> <span x-text="character.data.race.fullName"></span></div>
-                <div x-show="character.data.hair" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Hair:</strong> <span x-text="character.data.hair"></span></div>
-                <div x-show="character.data.eyes" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Eyes:</strong> <span x-text="character.data.eyes"></span></div>
-                <div x-show="character.data.skin" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Skin:</strong> <span x-text="character.data.skin"></span></div>
-                <div x-show="character.data.height" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Height:</strong> <span x-text="character.data.height"></span></div>
-                <div x-show="character.data.weight" class="flex items-center justify-between px-4 py-1 bg-slate-950"><strong>Weight:</strong> <span x-text="character.data.weight"></span></div>
+                <div x-show="character.data.gender" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Gender:</strong> <span x-text="character.data.gender"></span></div>
+                <div x-show="character.data.age" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Age:</strong> <span x-text="character.data.age"></span></div>
+                <div x-show="character.data.race.fullName" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Race:</strong> <span x-text="character.data.race.fullName"></span></div>
+                <div x-show="character.data.hair" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Hair:</strong> <span x-text="character.data.hair"></span></div>
+                <div x-show="character.data.eyes" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Eyes:</strong> <span x-text="character.data.eyes"></span></div>
+                <div x-show="character.data.skin" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Skin:</strong> <span x-text="character.data.skin"></span></div>
+                <div x-show="character.data.height" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Height:</strong> <span x-text="character.data.height"></span></div>
+                <div x-show="character.data.weight" class="flex items-center justify-between px-6 py-1 md:px-4 bg-slate-950"><strong>Weight:</strong> <span x-text="character.data.weight"></span></div>
               </div>
 
               <div x-show="character.data.notes.backstory" x-html="renderedContent" x-init="renderedContent = character.data.notes.backstory.split('\n').map(line => line.trim()).join('<br>')"></div>
