f9028bfe552ffc58a5ae0834ee2ac84ad332513e
bitbruder <business.fgaa@gmail.com>
2023-09-21 17:51:16 +0200

    add npcs templates and blueprints

---
+++ site/templates/npcs.php
@@ -0,0 +1,16 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+<?php snippet('hero') ?>
+
+<div class="max-w-[1920px] w-full my-6 lg:my-12 mx-auto px-6 md:px-8 flex flex-row">
+  <div class="w-full content">
+    <h1 class="text-[38px] font-cormorant"><?= $page->title() ?></h1>
+    <?= $page->text()->kirbytext() ?>
+
+    <div class="grid w-full grid-cols-1 gap-6 mt-6 mb-12 lg:mt-12 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+      <?php snippet('character-teaser', ['pageString' => 'npcs']) ?>
+    </div>
+  </div>
+</div>
+
+<?php snippet('footer') ?>
