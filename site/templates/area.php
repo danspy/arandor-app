e04f7e4a545c8381e9078ebfc54660c999781dbf
bitbruder <business.fgaa@gmail.com>
2023-10-05 11:42:38 +0200

    add areas templates and blueprints

---
+++ site/templates/area.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+<?php snippet('hero') ?>
+
+<div class="max-w-[1920px] w-full my-6 lg:my-12 mx-auto px-6 md:px-8 flex flex-row">
+  <div class="w-full content">
+    <h1 class="text-[38px] font-cormorant"><?= $page->title() ?></h1>
+    <?= $page->text()->kirbytext() ?>
+  </div>
+</div>
+
+<?php snippet('footer') ?>
