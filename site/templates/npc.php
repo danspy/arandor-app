02ccb1078843773cf6f453b8fd5dee142c25e1ee
Daniel Jähnichen <danieljahnichen@Daniels-Mini.fritz.box>
2023-10-09 23:27:31 +0200

    Adjust line-height of npc title

--- site/templates/npc.php
+++ site/templates/npc.php
@@ -15,7 +15,7 @@
         </div>
         
         <div class="flex items-center justify-center col-span-2 p-3">
-          <h1 class="text-[28px] font-cormorant lg:text-[38px]"><?= $page->title() ?></h1>
+          <h1 class="text-[28px] leading-[1.1] font-cormorant lg:text-[38px]"><?= $page->title() ?></h1>
         </div>
 
         <?php if($image = $page->image()) { ?>
@@ -24,7 +24,7 @@
       </div>
     <?php } else { ?>
       <div class="w-full px-6 lg:max-w-[900px] py-6 lg:py-12 lg:pl-12 lg:pr-12 lg:mx-auto">
-        <h1 class="text-[28px] font-cormorant lg:text-[38px]"><?= $page->title() ?></h1>
+        <h1 class="text-[28px] leading-[1.1] font-cormorant lg:text-[38px]"><?= $page->title() ?></h1>
       </div>
     <?php } ?>
 
