3107c577d2c72af729af39db2cf44bb1e99d23c6
Daniel Jähnichen <danieljahnichen@Daniels-Mini.fritz.box>
2023-10-10 11:45:54 +0200

    Add default and subpage template updates

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,9 +3,16 @@
 <?php snippet('hero') ?>
 
 <div class="max-w-[1920px] w-full my-6 lg:my-12 mx-auto px-6 md:px-8 flex flex-row">
-  <div class="content">
+  <div class="w-full content">
     <h1 class="text-[38px] font-cormorant"><?= $page->title() ?></h1>
     <?= $page->text()->kirbytext() ?>
+
+    <?php if ($page->hasChildren()) { ?>
+      <div class="grid w-full grid-cols-1 gap-6 mt-6 mb-12 lg:mt-12 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+        <?php snippet('npc-teaser', ['pageString' => $page->slug()]) ?>
+      </div>
+    <?php } ?>
+
   </div>
 </div>
 
---
+++ site/templates/subpage.php
@@ -0,0 +1,29 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+
+<div class="relative flex flex-col w-full lg:flex-row" >
+  <?php if ($page->hasSiblings() && count($page->siblings()) > 1) { ?>
+    <?php $parentPage = $page->parent(); if ($parentPage) { $parentTitle = $parentPage->title(); ?>
+      <?php snippet('sidemenu', ['submenuTitle' => $parentTitle]) ?>
+    <?php } ?>
+  <?php } ?>
+
+  <div class="w-full mb-12 content lg:mb-0">
+
+    <?php snippet('hero') ?>
+
+    <div class="w-full px-6 lg:max-w-[900px] lg:pb-12 lg:pl-12 lg:pr-12 lg:mx-auto">
+
+      <div class="pb-12 mt-12 mb-12 border-b-2 border-b-black">
+        <h2 class="text-[38px] font-cormorant leading-10">
+          <span class="break-words hyphens-auto"><?= $page->title() ?></span>
+        </h2>
+      </div>
+
+      <?= $page->text()->kirbytext() ?>
+    </div>
+
+  </div>
+</div>
+
+<?php snippet('footer') ?>
